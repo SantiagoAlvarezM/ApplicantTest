e8bb6a849da6499b41ad485c5098d133922f2bd6
Santiago Alvarez <stgoalvarezm@gmail.com>
2018-01-12 00:01:05 -0500

    refactor splash

--- app/src/main/java/me/santiagoalvarez/kogiaplicanttest/init/splash/SplashActivity.kt
+++ app/src/main/java/me/santiagoalvarez/kogiaplicanttest/init/splash/SplashActivity.kt
@@ -1,42 +1,29 @@
 package me.santiagoalvarez.kogiaplicanttest.init.splash
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.widget.Toast
 import me.santiagoalvarez.kogiaplicanttest.R
+import me.santiagoalvarez.kogiaplicanttest.common.base.BaseActivity
 import me.santiagoalvarez.kogiaplicanttest.home.MainActivity
 import me.santiagoalvarez.kogiaplicanttest.navigation.IntentNavigationEntry
-import me.santiagoalvarez.kogiaplicanttest.navigation.NavigationEntry
-import me.santiagoalvarez.kogiaplicanttest.navigation.Navigator
 
 /**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
+ * @author santiagoalvarez
  */
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity() {
 
-    lateinit var navigator: Navigator
-    private val mHideHandler = Handler()
-    private val mHidePart2Runnable = Runnable {
-        // Delayed navigation transaction
-
-        val entry = IntentNavigationEntry.Builder(navigator, MainActivity.createIntent(this))
+    private val handler = Handler()
+    private val runnable = Runnable {
+        IntentNavigationEntry.Builder(navigator, MainActivity.createIntent(this))
                 .clearStack()
-                .build()
-        navigator.navigateTo(entry)
+                .navigate()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_splash)
         supportActionBar?.hide()
-
-        navigator = Navigator(this, savedInstanceState)
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.postDelayed(mHidePart2Runnable, NAVIGATION_TRANSACTION_DELAY.toLong())
+        handler.postDelayed(runnable, NAVIGATION_TRANSACTION_DELAY.toLong())
     }
 
     companion object {

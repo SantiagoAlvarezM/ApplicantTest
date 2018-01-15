c863bd7efcb04a246efbb8a88c6854392a62018b
Santiago Alvarez <stgoalvarezm@gmail.com>
2018-01-14 22:27:23 -0500

    refactor scopes

--- app/src/main/java/me/santiagoalvarez/kogiaplicanttest/di/ActivityScoped.kt
+++ app/src/main/java/me/santiagoalvarez/kogiaplicanttest/di/ActivityScoped.kt
@@ -1,9 +1,5 @@
 package me.santiagoalvarez.kogiaplicanttest.di
 
-import java.lang.annotation.Documented
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
-
 import javax.inject.Scope
 
 /**
@@ -12,7 +8,7 @@ import javax.inject.Scope
  * scope to be used by all fragment components. Additionally, a component with a specific scope
  * cannot have a sub component with the same scope.
  */
-@Documented
+@kotlin.annotation.MustBeDocumented
 @Scope
-@Retention(RetentionPolicy.RUNTIME)
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
 annotation class ActivityScoped
--- app/src/main/java/me/santiagoalvarez/kogiaplicanttest/di/FragmentScoped.kt
+++ app/src/main/java/me/santiagoalvarez/kogiaplicanttest/di/FragmentScoped.kt
@@ -1,11 +1,9 @@
 package me.santiagoalvarez.kogiaplicanttest.di
 
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
-
 import javax.inject.Scope
 
 @Scope
-@Retention(RetentionPolicy.RUNTIME)
-@Target(AnnotationTarget.CLASS, AnnotationTarget.FILE, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FILE, AnnotationTarget.FUNCTION,
+        AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
 annotation class FragmentScoped

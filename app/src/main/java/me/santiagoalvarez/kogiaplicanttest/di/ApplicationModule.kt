a5be13670611cc5236b1c6bb2b9e2890641adbd6
Santiago Alvarez <stgoalvarezm@gmail.com>
2018-01-21 12:15:41 -0500

    added provides dependencies

--- app/src/main/java/me/santiagoalvarez/kogiaplicanttest/di/ApplicationModule.kt
+++ app/src/main/java/me/santiagoalvarez/kogiaplicanttest/di/ApplicationModule.kt
@@ -3,15 +3,24 @@ package me.santiagoalvarez.kogiaplicanttest.di
 import android.app.Application
 import android.content.Context
 import android.util.Log
-import com.twitter.sdk.android.core.*
+import com.twitter.sdk.android.core.DefaultLogger
+import com.twitter.sdk.android.core.TwitterConfig
 import dagger.Module
 import dagger.Provides
 import me.santiagoalvarez.kogiaplicanttest.auth.AccountType
 import me.santiagoalvarez.kogiaplicanttest.auth.ProfileManager
 import me.santiagoalvarez.kogiaplicanttest.auth.ProfileManagerImpl
 import me.santiagoalvarez.kogiaplicanttest.common.base.BaseActivity
+import me.santiagoalvarez.kogiaplicanttest.data.service.AuthInterceptor
+import me.santiagoalvarez.kogiaplicanttest.data.service.InstagramClient
+import me.santiagoalvarez.kogiaplicanttest.instagram.login.InstagramConfig.INSTAGRAM_API_BASE_URL
+import me.santiagoalvarez.kogiaplicanttest.instagram.login.InstagramLoginActivity
 import me.santiagoalvarez.kogiaplicanttest.navigation.Navigator
 import me.santiagoalvarez.kogiaplicanttest.twitter.login.TwitterLoginActivity
+import me.santiagoalvarez.kogiaplicanttest.util.PreferenceHelper
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 /**
@@ -21,22 +30,39 @@ import me.santiagoalvarez.kogiaplicanttest.twitter.login.TwitterLoginActivity
 class ApplicationModule {
 
     @Provides
-    fun providesContext(application: Application): Context = application
+    fun providesContext(application: Application): Context = application.applicationContext
 
     @Provides
-    fun providesProfileManager(): ProfileManager = ProfileManagerImpl()
+    fun providesPreferenceHelper(context: Context): PreferenceHelper = PreferenceHelper(context)
 
     @Provides
-    fun providesTwitterConfig(application: Application): TwitterConfig
-            = TwitterConfig.Builder(application)
+    fun providesProfileManager(preferenceHelper: PreferenceHelper): ProfileManager =
+            ProfileManagerImpl(preferenceHelper)
+
+    @Provides
+    fun providesTwitterConfig(application: Application): TwitterConfig = TwitterConfig.Builder(application)
             .logger(DefaultLogger(Log.DEBUG))
             .debug(true)
             .build()
 
     @Provides
+    fun providesRetrofitInstagramClient(profileManager: ProfileManager): InstagramClient {
+        val okHttpClientBuilder = OkHttpClient.Builder()
+        val authInterceptor = AuthInterceptor(profileManager.getInstagramAccessToken()!!)
+        if (!okHttpClientBuilder.interceptors().contains(authInterceptor)) {
+            okHttpClientBuilder.addInterceptor(authInterceptor)
+        }
+        return Retrofit.Builder()
+                .baseUrl(INSTAGRAM_API_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClientBuilder.build())
+                .build()
+                .create(InstagramClient::class.java)
+    }
+
+    @Provides
     fun providesNavigationListener(context: Context, profileManager: ProfileManager):
-            Navigator.NavigationListener
-            = Navigator.NavigationListener { navigator, entry ->
+            Navigator.NavigationListener = Navigator.NavigationListener { navigator, entry ->
         if (entry.isLoginRequired(AccountType.TWITTER) &&
                 !profileManager.isAuthenticated(AccountType.TWITTER)) {
             navigator.addPendingNavigation(BaseActivity.REQUEST_SIGNIN.toString(), entry)
@@ -46,8 +72,11 @@ class ApplicationModule {
             return@NavigationListener true
         } else if (entry.isLoginRequired(AccountType.INSTAGRAM) &&
                 !profileManager.isAuthenticated(AccountType.INSTAGRAM)) {
-            //TODO("not implemented")
-            return@NavigationListener false
+            navigator.addPendingNavigation(BaseActivity.REQUEST_SIGNIN.toString(), entry)
+            navigator.to(InstagramLoginActivity.createIntent(context))
+                    .withRequestCode(BaseActivity.REQUEST_SIGNIN)
+                    .navigate()
+            return@NavigationListener true
         }
         return@NavigationListener false
     }
